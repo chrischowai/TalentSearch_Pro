b3d6f09b53856f4f63ad5afe2856b77023a5bb5a
chrischowai <chrischowai@gmail.com>
2025-09-18 15:47:54 +0800

    Update CandidateViewCorner.tsx

--- src/components/CandidateViewCorner.tsx
+++ src/components/CandidateViewCorner.tsx
@@ -92,7 +92,7 @@ export const CandidateViewCorner: React.FC<CandidateViewCornerProps> = ({ candid
 
       {truncatedSnippet && (
         <div>
-          <p className="text-sm text-muted-foreground mb-1 font-bold">Snippet:</p>
+          <p className="text-sm text-muted-foreground mb-1 font-bold">Snippet from Linkedin:</p>
           <p className="text-xs text-foreground leading-relaxed ">{truncatedSnippet}</p>
         </div>
       )}
